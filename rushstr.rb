e8de46d26374324543d552d5df44fe64f4b57e6b
Frederick Alvarez Flores <hk.fred.0@gmail.com>
2025-05-15 20:07:24 -0700

    Create rushstr.rb

---
+++ rushstr.rb
@@ -0,0 +1,11 @@
+class Rushstr < Formula
+  desc "Interactive command history searcher"
+  homepage "https://github.com/donhk/rushstr"
+  url "https://github.com/donhk/rushstr/releases/download/v0.0.0/rushstr-0.0.0-x86_64-macos.tar.xz"
+  sha256 "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+  version "0.0.0"
+
+  def install
+    bin.install "rushstr"
+  end
+end
