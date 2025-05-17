408561d0afb3660d9c7a3806d3da957091c06317
Frederick Alvarez Flores <hk.fred.0@gmail.com>
2025-05-16 18:24:57 -0700

    updating formula for rushstr

--- rushstr.rb
+++ rushstr.rb
@@ -1,9 +1,9 @@
 class Rushstr < Formula
   desc "Interactive command history searcher"
   homepage "https://github.com/donhk/rushstr"
-  url "https://github.com/donhk/rushstr/releases/download/v1.4.0/rushstr-v1.4.0-aarch64-macos.tar.xz"
-  sha256 "1adee468f9bea668703eaa414720d07ab2b03d5beda0efb9b7365fffc602dab0"
-  version "1.4.0"
+  url "https://github.com/donhk/rushstr/releases/download/v1.4.1/rushstr-v1.4.1-aarch64-macos.tar.xz"
+  sha256 "8d4dcce68298f376c44adbc429f68c0362798e77eb23e6332f041e6e8e3966da"
+  version "1.4.1"
 
   def install
     bin.install "rushstr"
